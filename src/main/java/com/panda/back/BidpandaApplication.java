016ab35d320bf12f086831d88c21d25252bc8369
dewey89 <140939516+dewey-89@users.noreply.github.com>
2023-10-04 20:21:17 +0900

    first commit

---
+++ src/main/java/com/panda/back/BidpandaApplication.java
@@ -0,0 +1,13 @@
+package com.panda.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidpandaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidpandaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/panda/back/BidpandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panda.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidpandaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
