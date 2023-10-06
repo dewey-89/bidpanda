f1c91b55e2d7372d1af56d6e1810949fa3e0ee64
dewey89 <140939516+dewey-89@users.noreply.github.com>
2023-10-06 21:48:09 +0900

    #5 - 상품등록 구현

--- src/main/java/com/panda/back/BidpandaApplication.java
+++ src/main/java/com/panda/back/BidpandaApplication.java
@@ -2,12 +2,15 @@ package com.panda.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BidpandaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BidpandaApplication.class, args);
+
     }
 
 }
