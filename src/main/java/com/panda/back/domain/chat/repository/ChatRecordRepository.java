e64272b37dcb50167e5415ccbfdc4794945cf8c0
OolongTea620 <rlafls9596@gmail.com>
2023-10-11 16:57:46 +0900

    #58 - chat기능 가져오기

---
+++ src/main/java/com/panda/back/domain/chat/repository/ChatRecordRepository.java
@@ -0,0 +1,14 @@
+package com.panda.back.domain.chat.repository;
+
+import com.panda.back.domain.chat.model.ChatRecord;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ChatRecordRepository extends MongoRepository<ChatRecord, String> {
+    @Query(value = "{roomId:?0}")
+    Optional<ChatRecord> findChatRecordByRoomIdEquals(String roomId);
+}
---
+++ src/main/java/com/panda/back/domain/chat/service/ChatRecordService.java
@@ -0,0 +1,34 @@
+package com.panda.back.domain.chat.service;
+
+
+import com.panda.back.domain.chat.model.ChatMessage;
+import com.panda.back.domain.chat.model.ChatRecord;
+import com.panda.back.domain.chat.repository.ChatRecordRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ChatRecordService {
+    private final ChatRecordRepository chatRecordRepository;
+
+    @Transactional
+    public void recordMessage(ChatMessage message) {
+        ChatRecord chatRecord;
+
+        Optional<ChatRecord> recordRoom = chatRecordRepository
+                .findChatRecordByRoomIdEquals(message.getRoomId());
+
+        if (recordRoom.isEmpty()) {
+            chatRecord = chatRecordRepository.insert(new ChatRecord(message.getRoomId()));
+        }else {
+            chatRecord = recordRoom.get();
+        }
+        chatRecord.recordMessage(message);
+        chatRecordRepository.save(chatRecord);
+    }
+
+}
