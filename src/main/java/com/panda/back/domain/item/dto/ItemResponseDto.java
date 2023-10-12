49d7fe6c8a65595250a4aeb7716863a6ee5831b7
byunggwan-Kim <ktera1231@gmail.com>
2023-10-12 21:01:06 +0900

    #60 - 단일 게시글 조회 시, 상품 등록 시 nickname 추가

--- src/main/java/com/panda/back/domain/item/dto/ItemResponseDto.java
+++ src/main/java/com/panda/back/domain/item/dto/ItemResponseDto.java
@@ -1,6 +1,7 @@
 package com.panda.back.domain.item.dto;
 
 import com.panda.back.domain.item.entity.Item;
+import com.panda.back.domain.member.entity.Member;
 import lombok.Getter;
 
 import java.net.URL;
@@ -17,6 +18,7 @@ public class ItemResponseDto {
     private Long minBidPrice;
     private LocalDateTime auctionEndTime;
     private List<URL> itemImages;
+    private String nickname;
 
     public ItemResponseDto(Item item){
         this.id = item.getId();
@@ -26,6 +28,17 @@ public class ItemResponseDto {
         this.minBidPrice = item.getMinBidPrice();
         this.auctionEndTime = item.getAuctionEndTime();
         this.itemImages = item.getImages();
+    }
+
+    public ItemResponseDto(Item item, String nickname){
+        this.id = item.getId();
+        this.title = item.getTitle();
+        this.content = item.getContent();
+        this.presentPrice = item.getPresentPrice();
+        this.minBidPrice = item.getMinBidPrice();
+        this.auctionEndTime = item.getAuctionEndTime();
+        this.itemImages = item.getImages();
+        this.nickname = nickname;
     }
 
     public static List<ItemResponseDto> listOf(List<Item> items) {
