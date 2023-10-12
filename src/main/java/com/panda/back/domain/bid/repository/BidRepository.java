ad4dd1b32e36217572326df6faed0277dc09c06d
dewey89 <140939516+dewey-89@users.noreply.github.com>
2023-10-12 18:16:23 +0900

    Changes by KBK

--- src/main/java/com/panda/back/domain/bid/repository/BidRepository.java
+++ src/main/java/com/panda/back/domain/bid/repository/BidRepository.java
@@ -1,8 +1,10 @@
 package com.panda.back.domain.bid.repository;
 
 import com.panda.back.domain.bid.entity.Bid;
+import com.panda.back.domain.item.entity.Item;
 import com.panda.back.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
