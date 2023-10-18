f70e6d84e85b118d508c8e49be9619a0259966f7
OolongTea620 <rlafls9596@gmail.com>
2023-10-19 01:03:06 +0900

    @Payload 추가, SimpMessagingTemplate 로 교체

--- src/main/java/com/panda/back/domain/chat/controller/MessageController.java
+++ src/main/java/com/panda/back/domain/chat/controller/MessageController.java
@@ -2,16 +2,12 @@ package com.panda.back.domain.chat.controller;
 
 import com.panda.back.domain.chat.dto.ReceiveMessage;
 import com.panda.back.domain.chat.dto.SendMessage;
-import com.panda.back.domain.chat.entity.component.Message;
 import com.panda.back.domain.chat.service.ChatRecordService;
-import com.panda.back.domain.chat.type.MessageType;
-import com.panda.back.domain.member.entity.Member;
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,11 +19,11 @@ import java.util.List;
 @Slf4j
 public class MessageController {
 
-    private final SimpMessageSendingOperations sendingOperations;
+    private final SimpMessagingTemplate sendingOperations;
     private final ChatRecordService chatRecordService;
 
     @MessageMapping("/chat/message") // ws://~/app/chat/message
-    public void enter(ReceiveMessage message) {
+    public void enter(@Payload ReceiveMessage message) {
         log.info("{}", message.toString());
         switch (message.getType()) {
             case TEXT, MEDIA -> chatRecordService.recordMessage(message);
