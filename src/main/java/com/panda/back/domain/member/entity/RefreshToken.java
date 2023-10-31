310098da412637c587ebc00c43936e1b61cdb05f
IfOnly <jc_22@naver.com>
2023-11-01 00:16:54 +0900

    #97 - RefreshTokenRepository, RefreshToken 수정

--- src/main/java/com/panda/back/domain/member/entity/RefreshToken.java
+++ src/main/java/com/panda/back/domain/member/entity/RefreshToken.java
@@ -1,33 +1,31 @@
 package com.panda.back.domain.member.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
 
 @Getter
-@AllArgsConstructor
-@RedisHash(value = "refreshtoken", timeToLive = 60 * 60 * 24 * 7)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@RedisHash(value = "refreshToken", timeToLive = 7 * 24 * 60 * 60)
 public class RefreshToken {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "token", nullable = false)
-    private String refreshtoken;
-
-    @Column(name = "membername", nullable = false)
+    @Indexed
+    private String token;
+    @Indexed
     private String membername;
 
+
     public RefreshToken(String token, String membername) {
-        this.refreshtoken = token;
+        this.token = token;
         this.membername = membername;
     }
 
     public void updateToken(String token) {
-        this.refreshtoken = token;
+        this.token = token;
     }
 }
