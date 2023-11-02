7e0a00f43bd0cfb682b9f88729b71c8a89bba5b6
dewey89 <140939516+dewey-89@users.noreply.github.com>
2023-11-02 20:20:49 +0900

    채팅방 입장시 nickname 추가해서 전송2

--- src/main/java/com/panda/back/domain/chat/dto/SendMessage.java
+++ src/main/java/com/panda/back/domain/chat/dto/SendMessage.java
@@ -50,7 +50,7 @@ public class SendMessage {
         switch (message.getType()) {
             case ENTER -> {
                 toClient.setProfileUrl(message.getProfileUrl());
-                toClient.setSender(message.getSender());
+                toClient.setSender(message.getNickname());
             }
             case TEXT -> {
                 toClient.setContent(message.getContent());
